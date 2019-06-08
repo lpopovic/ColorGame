9a4ae4eff88268bd6a0121e34e9a3c03dcf08ec0
Lazar Popovic <lazar.popovic.m@gmail.com>
2019-06-09 00:50:55 +0200

    Good code swift

--- ColorGame/GameScene.swift
+++ ColorGame/GameScene.swift
@@ -9,172 +9,93 @@
 import SpriteKit
 import GameplayKit
 
-enum Enemies: Int {
-    case small
-    case medium
-    case large
-}
-
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    var tracksArray: [SKSpriteNode]? = [SKSpriteNode]()
-    var player: SKSpriteNode?
+    // MARK: Game Properties
+    // MARK:-
+    
+    // MARK: Collision Categories
+    let playerCategory:UInt32 = 0x1 << 0
+    let enemyCategory:UInt32 = 0x1 << 1
+    let targetCategory:UInt32 = 0x1 << 2
+    let powerUpCategory:UInt32 = 0x1 << 3
+    
+    // MARK: Nodes
+    var player:SKSpriteNode?
     var target:SKSpriteNode?
     
-    var currenttrack = 0
-    var movingToTrack = false
     
-    let moveSound = SKAction.playSoundFileNamed("move.wav", waitForCompletion: false)
+    // MARK: HUD
+    var timeLabel:SKLabelNode?
+    var scoreLabel:SKLabelNode?
+    var currentScore:Int = 0 {
+        didSet {
+            self.scoreLabel?.text = "SCORE: \(self.currentScore)"
+        }
+    }
+    var remainingTime:TimeInterval = 60 {
+        didSet {
+            self.timeLabel?.text = "TIME: \(Int(self.remainingTime))"
+        }
+    }
     
+    
+    
+    // MARK: Arrays
+    var tracksArray:[SKSpriteNode]? = [SKSpriteNode]()
     let trackVelocities = [180, 200, 250]
     var directionArray = [Bool]()
     var velocityArray = [Int]()
     
-    let playerCategory: UInt32 = 0x1 << 0
-    let enemyCategory: UInt32 = 0x1 << 1
-    let targetCategory: UInt32 = 0x1 << 2
+    // MARK: Sound
     
-    func setupTracks(){
-        for i in 0 ... 8 {
-            if let track = self.childNode(withName: "\(i)") as? SKSpriteNode {
-                tracksArray?.append(track)
-            }
-        }
-    }
-
-    func createPlayer() {
-        player = SKSpriteNode(imageNamed: "player")
-        player?.physicsBody = SKPhysicsBody(circleOfRadius: player!.size.width / 2)
-        player?.physicsBody?.linearDamping = 0
-        player?.physicsBody?.categoryBitMask = playerCategory
-        player?.physicsBody?.collisionBitMask = 0 // deactive  all colision
-        player?.physicsBody?.contactTestBitMask = enemyCategory | targetCategory // notif when colision player
-        
-        guard let playerPosition = tracksArray?.first?.position.x else {
-            return
-        }
-        player?.position = CGPoint(x: playerPosition, y: self.size.height / 2)
-        
-        self.addChild(player!)
-        
-        let pulse = SKEmitterNode(fileNamed: "pulse")!
-        player?.addChild(pulse)
-        pulse.position = CGPoint(x: 0, y: 0)
-        
-    }
-    func createTarget() {
-        target = self.childNode(withName: "target") as? SKSpriteNode
-        target?.physicsBody = SKPhysicsBody(circleOfRadius: target!.size.width / 2)
-        target?.physicsBody?.categoryBitMask = targetCategory
-        target?.physicsBody?.collisionBitMask = 0 // nece se pomerati prilikom udara
-    }
+    let moveSound = SKAction.playSoundFileNamed("move.wav", waitForCompletion: false)
+    var backgroundNoise:SKAudioNode!
     
-    func createEnemy (type: Enemies, forTrack track:Int) -> SKShapeNode? {
-        
-        let enemySprite = SKShapeNode()
-        enemySprite.name = "ENEMY"
-        
-        switch type {
-        case .small:
-            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 70), cornerWidth: 8, cornerHeight: 8, transform: nil)
-            enemySprite.fillColor = UIColor(red: 0.4431, green: 0.5529, blue: 0.7451, alpha: 1)
-        case .medium:
-            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 100), cornerWidth: 8, cornerHeight: 8, transform: nil)
-            enemySprite.fillColor = UIColor(red: 0.7804, green: 0.4039, blue: 0.4039, alpha: 1)
-        case .large:
-            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 130), cornerWidth: 8, cornerHeight: 8, transform: nil)
-            enemySprite.fillColor = UIColor(red: 0.7804, green: 0.6392, blue: 0.4039, alpha: 1)
     
-        }
-        
-        guard let enemyPosition = tracksArray?[track].position else {
-            return nil
-        }
-        
-        let up = directionArray[track]
-        
-        enemySprite.position.x = enemyPosition.x
-        enemySprite.position.y = up ? -130 : self.size.height + 130
-        
-        enemySprite.physicsBody = SKPhysicsBody(edgeLoopFrom: enemySprite.path!)
-        enemySprite.physicsBody?.categoryBitMask = enemyCategory
-        enemySprite.physicsBody?.velocity = up ? CGVector(dx: 0, dy: velocityArray[track]) : CGVector(dx: 0, dy: -velocityArray[track])
-        
-        return enemySprite
-        
-    }
+    // MARK: Rest
+    var currentTrack = 0
+    var movingToTrack = false
     
-    func spwanEnemies() {
-        for i in 1 ... 7 {
-            let randomEnemyType = Enemies(rawValue: GKRandomSource.sharedRandom().nextInt(upperBound: 3))!
-            if let newEnemy = createEnemy(type: randomEnemyType, forTrack: i) {
-                self.addChild(newEnemy)
-            }
-        }
-        
-        // delete enemy koji se nevide
-        self.enumerateChildNodes(withName: "ENEMY") { (node, nil) in
-            if node.position.y < -150 || node.position.y > self.size.height + 150 {
-                node.removeFromParent()
-            }
-        }
-        
-    }
+    
+    
+    
+    
+    // MARK: - Entry Point
     
     override func didMove(to view: SKView) {
-        
         setupTracks()
+        
+        createHUD()
+        launchGameTimer()
+        
         createPlayer()
         createTarget()
         
         self.physicsWorld.contactDelegate = self
         
+        if let musicURL = Bundle.main.url(forResource: "background", withExtension: "wav") {
+            backgroundNoise = SKAudioNode(url: musicURL)
+            addChild(backgroundNoise)
+        }
+        
+        
         if let numberOfTracks = tracksArray?.count {
             for _ in 0 ... numberOfTracks {
-               
-                let randowmNumberForVelocity = GKRandomSource.sharedRandom().nextInt(upperBound: 3) // 0 do 2
-                velocityArray.append(trackVelocities[randowmNumberForVelocity])
+                let randomNumberForVelocity = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+                velocityArray.append(trackVelocities[randomNumberForVelocity])
                 directionArray.append(GKRandomSource.sharedRandom().nextBool())
             }
         }
-       
-        // call loop
-        self.run(SKAction.repeatForever(SKAction.sequence([SKAction.run({
+        
+        
+        self.run(SKAction.repeatForever(SKAction.sequence([SKAction.run {
             self.spwanEnemies()
-        }), SKAction.wait(forDuration: 2)])))
+            }, SKAction.wait(forDuration: 2)])))
         
     }
     
-    func moveVertically (up:Bool) {
-        if up {
-            let moveAction = SKAction.moveBy(x: 0, y: 3, duration: 0.01)
-            let repeatAction = SKAction.repeatForever(moveAction)
-            player?.run(repeatAction)
-        } else {
-            let moveAction = SKAction.moveBy(x: 0, y: -3, duration: 0.01)
-            let repeatAction = SKAction.repeatForever(moveAction)
-            player?.run(repeatAction)
-        }
-    }
-    
-    func moveToNextTrack() {
-        player?.removeAllActions()
-        movingToTrack = true
-        
-        guard let nextTrack = tracksArray?[currenttrack + 1].position else {
-            return
-        }
-        
-        if let player = player {
-            let moveAction = SKAction.move(to: CGPoint(x: nextTrack.x, y: player.position.y), duration: 0.2)
-            player.run(moveAction) {
-                self.movingToTrack = false
-            }
-            currenttrack += 1
-            
-            self.run(moveSound)
-        }
-    }
+    // MARK: - Touch Control
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
@@ -183,58 +104,67 @@ class GameScene: SKScene {
             
             if node?.name == "right" {
                 moveToNextTrack()
-                
             } else if node?.name == "up" {
                 moveVertically(up: true)
-               
             } else if node?.name == "down" {
                 moveVertically(up: false)
-                
             }
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        player?.removeAllActions()
-        
         if !movingToTrack {
             player?.removeAllActions()
         }
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         player?.removeAllActions()
-
     }
     
     
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-}
-
-extension GameScene: SKPhysicsContactDelegate {
+    // MARK: - Contact Delegate
     
     func didBegin(_ contact: SKPhysicsContact) {
-        var playerBody: SKPhysicsBody
-        var otherBody: SKPhysicsBody
+        var playerBody:SKPhysicsBody
+        var otherBody:SKPhysicsBody
         
         if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
             playerBody = contact.bodyA
             otherBody = contact.bodyB
-            
-            
-        } else {
+        }else{
             playerBody = contact.bodyB
             otherBody = contact.bodyA
         }
         
-        if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask  == enemyCategory {
-            print("Enemey hit")
-        }else if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask  == targetCategory {
-            print("Target hit")
+        if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask == enemyCategory {
+            self.run(SKAction.playSoundFileNamed("fail.wav", waitForCompletion: true))
+            movePlayerToStart()
+            
+        } else if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask == targetCategory {
+            nextLevel(playerPhysicsBody: playerBody)
+        } else if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask == powerUpCategory {
+            self.run(SKAction.playSoundFileNamed("powerUp.wav", waitForCompletion: true))
+            otherBody.node?.removeFromParent()
+            remainingTime += 5
         }
+        
+        
     }
     
+    
+    // MARK: - Update
+    
+    override func update(_ currentTime: TimeInterval) {
+        if let player = self.player {
+            if player.position.y > self.size.height || player.position.y < 0 {
+                movePlayerToStart()
+            }
+        }
+        
+        if remainingTime <= 5 {
+            timeLabel?.fontColor = UIColor.red
+        }
+    }
 }
