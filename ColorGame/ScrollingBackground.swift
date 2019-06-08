dcda6524c0cb5a4baa2f2f8320f8a7af98ea371f
Lazar Popovic <lazar.popovic.m@gmail.com>
2019-06-09 01:25:40 +0200

    ScrollingBackground

---
+++ ColorGame/ScrollingBackground.swift
@@ -0,0 +1,14 @@
+//
+//  ScrollingBackground.swift
+//  ColorGame
+//
+//  Created by MacBook Air on 6/9/19.
+//  Copyright © 2019 Riitech. All rights reserved.
+//
+
+
+import SpriteKit
+
+class ScrollingBackground: SKSpriteNode {
+
+}
